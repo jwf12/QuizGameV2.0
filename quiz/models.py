121157246b9bba0526295b287806e12aabe8f634
jwf12 <jwf_12@hotmail.com>
2023-02-14 20:32:47 -0300

    Se crearon template, y los modelos.

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateField(auto_now=True)
+    update_at = models.DateField(auto_now=True)
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=50)
+    is_correct = models.BooleanField(default=False)
+
+
+class UserProgress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.PositiveIntegerField(default=0)
+    complated_at = models.DateField(auto_now=True)
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from .models import Quiz, Question, Answer, UserProgress
+from django.views import generic
 # Create your views here.
+
