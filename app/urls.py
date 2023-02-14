5efd2eb80c4032a3306af3c1820b6ed8f9c537fe
jwf12 <jwf_12@hotmail.com>
2023-02-14 20:50:32 -0300

    Se arreglo bug con url

--- app/urls.py
+++ app/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include(('quiz.urls', 'auth'), namespace='auth')),
+    path('auth/', include('quiz.urls'), name='auth'),
 
 ]
--- quiz/urls.py
+++ quiz/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 
-
+app_name = 'quiz'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'login.html')),
-    path('register/', auth_views.LoginView.as_view(template_name = 'register.html')),
+    path('register/', auth_views.LoginView.as_view(template_name = 'register.html'), name='register'),
 
 ]
