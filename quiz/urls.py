c10b97a93e320309052c6b42b913808228e3fdd9
jwf12 <jwf_12@hotmail.com>
2023-02-17 20:35:31 -0300

    Se arreglo el problema de la url

--- quiz/urls.py
+++ quiz/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name = 'login'),
     path('register/', SingUpView.as_view(), name='register'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'login.html'), name = 'logout'),
-    path('questions/<int:id>/', QuestionView.as_view(), name = 'question_list'),
+    path('questions/<int:quiz_id>/', QuestionView.as_view(), name = 'question_list'),
     path('', Home.as_view(), name = 'home'),
 ]
