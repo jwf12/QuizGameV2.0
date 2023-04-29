f1d39e89ffc3875e768261bff01f454bbe99c391
jwf12 <jwf_12@hotmail.com>
2023-04-29 18:44:49 -0300

    vercel deploy

--- app/settings.py
+++ app/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^wj474@ox@nn0pae!)a*0o(m!4jjpal9f!m&won)(^ibrl=trf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
